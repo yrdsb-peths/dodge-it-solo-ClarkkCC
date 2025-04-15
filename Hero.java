0a9cf26d7f62e7afe51ecc80cf59463071c9d8cd
ClarkkCC <Clark113008@gmail.com>
2025-04-15 14:22:37 -0400

    Add comment

---
+++ Hero.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Hero extends Actor
+{
+    //one line of comment//
+     public void act()
+    {
+        // Add your action code here.
+    }
+}
