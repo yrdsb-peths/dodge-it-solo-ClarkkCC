cdde3b1adaf9fffba62d2a9b851968cae624ef7b
ClarkkCC <Clark113008@gmail.com>
2025-04-15 14:30:20 -0400

    Add hero

--- Hero.java
+++ Hero.java
@@ -3,9 +3,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Hero extends Actor
 {
-    //one line of comment//
+    boolean atTop = true;
      public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.mouseClicked(null))
+        {
+            atTop = !atTop;
+        }
+        
+        if(atTop) 
+        {
+            setLocation(100,100);
+        }
+        else
+        {
+            setLocation(100,300);
+        }
     }
 }
