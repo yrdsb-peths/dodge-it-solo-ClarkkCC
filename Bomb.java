a6ecd57a8383c6027f5c7707d61d9b7303fd6648
ClarkkCC <Clark113008@gmail.com>
2025-04-15 14:38:26 -0400

    Add bomb

---
+++ Bomb.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+
+public class Bomb extends Actor
+{
+ 
+    public void act()
+    {
+        move(-10);
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -3,7 +3,10 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        Hero hero =  new Hero();
+        ClarkKent hero =  new ClarkKent();
         addObject(hero,100,100);
+        
+        Bomb meteor = new Bomb();
+        addObject(meteor, 600, 100); 
     }
 }
